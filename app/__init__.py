0ba9075da6823ff85cb86a5d1fb8c8d6155d8755
Shubh1u <105773461+Shubh1u@users.noreply.github.com>
2025-09-02 22:48:34 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,15 +8,12 @@ jwt = JWTManager()
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config.Config')
+    app.secret_key = "supersecret"
 
     db.init_app(app)
     jwt.init_app(app)
 
-    from app.routes import auth, student, achievement, validation, portfolio
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(student.bp)
-    app.register_blueprint(achievement.bp)
-    app.register_blueprint(validation.bp)
-    app.register_blueprint(portfolio.bp)
+    from app.routes.auth import bp as auth_bp
+    app.register_blueprint(auth_bp)
 
     return app
