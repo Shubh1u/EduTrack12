5de74a5474d8975773b1f6a916089d5cb9186698
Shubh1u <105773461+Shubh1u@users.noreply.github.com>
2025-09-02 18:56:01 +0530

    Update auth.py

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -14,7 +14,7 @@ def register():
     if request.method == 'GET':
         return render_template('register.html')  # Show the form
 
-    data = request.json or request.form
+    data = request.form
     user = User(
         name=data['name'],
         email=data['email'],
@@ -29,7 +29,7 @@ def login():
     if request.method == 'GET':
         return render_template('login.html')  # Show the form
 
-    data = request.json or request.form
+    data = request.form
     user = User.query.filter_by(email=data['email']).first()
     if user and check_password(data['password'], user.password):
         return jsonify({"msg": "Login successful"}), 200
